bee66481d9274f6cab35873ae61dadd39ab4a6e1
Hongzhang Xue <xuehzh95@qq.com>
2024-11-11 15:26:08 +0800

    Delete GeneLevel_PangenomeBuild directory

--- GeneLevel_PangenomeBuild/ProteinQC/PerTranscriptPerGene/ptpg.sh
+++
@@ -1,4 +0,0 @@
-# ptpg.py is from EUPAN3 project, https://github.com/zhixue/EUPAN3
-# get the longest protein (CDS) transcript as the representative from each gene
-# it can remove the effect of several proteins of one gene when comparing with other samples (such as the sample with only one transcript in each gene)
-python3 ~/tools/EUPAN3/lib/ptpg.py -i x.gtf/x.gff3 -r cds -o x_pTpG.gtf/x_pTpG.gff
